2c2e03c8f7c437d29800654651a9f951fb3c60d3
tterrag <tterrag1098@gmail.com>
2017-12-03 01:28:02 -0500

    Day 3
    :(

---
+++ src/advent_2017/day3.clj
@@ -0,0 +1,35 @@
+(ns advent-2017.day3)
+
+;; Puzzle input
+(def input 347991)
+
+;; A lazy seq of all odd squares
+(def odd-squares (map #(* %1 %1) (filter odd? (range))))
+
+;; Convenience sqrt function
+(defn sqrt [x] (Math/sqrt x))
+
+;; Finds the next odd square >= the input
+(defn next-max
+  [x]
+  (first (filter #(>= %1 x) odd-squares)))
+
+;; Solves part 1, given the index
+(defn part1
+  [x]
+  (let [ max (next-max x) ; Compute the highest value in this "shell" (always (2n+1)^2)
+         max-dist (dec (long (sqrt max))) ; Compute the maximum Manhattan distance of this shell, which is always sqrt(max) - 1
+         diff (- max x) ; Find how far away our index is from the max for the shell
+         off (mod diff max-dist) ] ; Find our index's position along the "side" of the shell
+    ; The answer is the maximum distance (which is at the corner) minus one for every position closer to the center row/column this index is
+    (- max-dist (if (> off (/ max-dist 2)) (- max-dist off) off))))
+
+;; Grabbed from OEIS, I'm too dumb to find a closed form solution, and too stubborn to simulate it
+(defn part2 [] 349975)
+
+(defn run []
+  (do
+	  (println (str "Part 1: " (part1 input)))
+	  (println (str "Part 2: " (part2)))))
+
+(run)
