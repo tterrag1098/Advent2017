ca040ea68718a6b1ccedc2994fb06b9e3ce38a03
tterrag <tterrag1098@gmail.com>
2017-12-04 00:16:46 -0500

    Day 4

---
+++ src/advent_2017/day4.clj
@@ -0,0 +1,19 @@
+(ns advent-2017.day4)
+(require '[clojure.string :as str])
+
+(def input (map #(str/split %1 #"\s") (str/split-lines (slurp "resources/day4.txt"))))
+
+(defn part1
+  [in]
+  (count (filter #(apply distinct? %) input)))
+
+(defn part2
+  [in]
+  (count (filter #(apply distinct? %) (map #(map sort %) input))))
+
+(defn run []
+  (do
+	  (println (str "Part 1: " (part1 input)))
+	  (println (str "Part 2: " (part2 input)))))
+
+(run)
