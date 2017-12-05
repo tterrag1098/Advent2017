f5350d57f4d958a8214873d09aeb395a1bf48d1a
tterrag <tterrag1098@gmail.com>
2017-12-05 14:13:25 -0500

    Fix typos in day 5

--- src/advent_2017/day05.clj
+++ src/advent_2017/day05.clj
@@ -3,16 +3,16 @@
 
 (def input (vec (map #(Integer/parseInt %) (str/split-lines (slurp "resources/day5.txt")))))
 
-(defn jump [in f] (jump-r in 0 0 f))
-
 (defn jump-r [in idx acc f]
  (if (>= idx (count in))
    acc
    (let [val (nth in idx)]
-     (recur (assoc in idx (f val)) (+ idx val) (inc acc)))))
+     (recur (assoc in idx (f val)) (+ idx val) (inc acc) f))))
+
+(defn jump [in f] (jump-r in 0 0 f))
 
 (defn part1 [in] (jump in inc))
-(defn part2 [in] #(if (>= val 3) (dec val) (inc val)))
+(defn part2 [in] (jump in #(if (>= % 3) (dec %) (inc %))))
 
 (do
   (println (str "Part 1: " (part1 input)))
