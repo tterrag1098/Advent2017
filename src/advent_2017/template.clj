73efb134b9a70c27e151698623e08fb88a65b9b7
tterrag <tterrag1098@gmail.com>
2017-12-06 00:34:57 -0500

    Add template

---
+++ src/advent_2017/template.clj
@@ -0,0 +1,13 @@
+(ns advent-2017.template)
+(require '[clojure.string :as str])
+(require '[advent_2017.core :as core])
+
+(def input (str/split-lines (slurp "resources/day6.txt")))
+
+(defn part1 [in]
+  )
+
+(defn part2 [in]
+  )
+
+(core/do-parts part1 part2 input)
