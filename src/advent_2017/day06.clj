c9df0ed306cb6e09fe530253e4de0befb94d8054
tterrag <tterrag1098@gmail.com>
2017-12-06 00:44:25 -0500

    Clean up and comment day 6

--- src/advent_2017/day06.clj
+++ src/advent_2017/day06.clj
@@ -4,27 +4,31 @@
 
 (def input (vec (map #(Integer/parseInt %) (str/split "11 11 13 7 0 15 5 5 4 4 1 1 7 1 15 11" #"\s"))))
 
+;; Find the first index matching pred in coll
 (defn first-idx [pred coll]
   (first (keep-indexed #(when (pred %2) %1) coll)))
 
+;; Increment i, modulo the size of coll
 (defn inc-wrap [i coll] 
   (mod (inc i) (count coll)))
 
+;; Distribute amt memory blocks into blocks, starting at position idx
 (defn distr [blocks idx amt]
   (if (zero? amt)
     blocks
-    (do
-      (recur (assoc blocks idx (inc (nth blocks idx))) (inc-wrap idx blocks) (dec amt)))))
+    (recur (assoc blocks idx (inc (nth blocks idx))) (inc-wrap idx blocks) (dec amt))))
 
 (defn part1 [in]
-  ((fn redis [blocks history acc]
-     (let [ idx (first-idx #(= (apply max blocks) %) blocks)
-            val (nth blocks idx) ]
-       (if (contains? history blocks)
-         { :steps acc :result blocks }
+  ((fn redis [blocks history acc] ; Redistribute memory blocks recursively
+     (let [ idx (first-idx #(= (apply max blocks) %) blocks) ; Find the first instance of the max value
+            val (nth blocks idx) ] ; Grab that max value
+       (if (contains? history blocks) ; If this state is already in the history, we are finished
+         { :steps acc :result blocks } ; So return a map of the current state
+         ; Otherwise, set the current index to 0, call distr with index+1 (wrapped), add this state to the history, and recurse
          (recur (distr (assoc blocks idx 0) (inc-wrap idx blocks) val) (conj history blocks) (inc acc)))))
   in #{} 0))
 
+;; Wonderful hack, just call part 1 twice, second time on the first time's output!
 (defn part2 [in]
   (part1 ((part1 in) :result)))
 
