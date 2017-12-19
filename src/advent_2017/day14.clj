53c1b6667d528b0c7a464e860d755ebf9688bd17
tterrag <tterrag1098@gmail.com>
2017-12-19 04:38:58 -0500

    Day 14 complete!

--- src/advent_2017/day14.clj
+++ src/advent_2017/day14.clj
@@ -3,16 +3,14 @@
 (require '[advent-2017.core :as core])
 (require '[advent-2017.day10 :as day10])
 
-(def input "jzgqcdpd")
+(def input "xlqgujun")
 
 (def hash-inputs (map #(str input "-" %) (range 128)))
 
-(def hashes (map #(day10/dense-hash
-                    (day10/do-hash day10/data (day10/get-lengths %) 64))
-                 hash-inputs))
+(def hashes (map #(day10/dense-hash (day10/do-hash day10/data (day10/get-lengths %) 64)) hash-inputs))
 
 (def grid 
-  (map (fn [n] (apply str (map #(str/replace (format "%8s" (Integer/toBinaryString %)) \  \0) n))) hashes))
+  (vec (map (fn [n] (vec (apply str (map #(str/replace (format "%8s" (Integer/toBinaryString %)) \  \0) n)))) hashes)))
 
 (defn part1 [in] (reduce + (map (fn [s] (count (filter #(= \1 %) s))) in)))
 
@@ -26,24 +24,25 @@
     (if (= val \1) (conj seen [x y]) seen)))
 
 (defn explore-group
-  ([grid seen x y] (explore-group grid seen (list [x y])))
-  ([grid seen search]
-    (if (empty? search) seen
+  ([grid seen x y id] 
+    (if (contains? seen [x y]) [grid seen] (explore-group grid seen (list [x y]) id)))
+  ([grid seen search id]
+    (if (empty? search) [grid seen]
       (let [ [x y] (peek search) ]
-        (if (= \0 (grid-get grid x y)) (recur grid seen (pop search))
+        (if (= \0 (grid-get grid x y)) (recur grid seen (pop search) id)
           (let [ found (-> #{}
 						             (add-if-present grid (inc x) y)
 						             (add-if-present grid (dec x) y)
 						             (add-if-present grid x (inc y))
 						             (add-if-present grid x (dec y))) 
                  new (filter #(not (contains? seen %)) found) ]
-            (do (prn x y new) (recur grid (clojure.set/union seen found) (if (empty? new) (pop search) (apply conj (pop search) new))))))))))
+            (recur (assoc grid y (assoc (nth grid y) x id)) (conj (clojure.set/union seen found) (peek search)) (if (empty? new) (pop search) (apply conj (pop search) new)) id)))))))
 
 (defn part2 [in]
   ((fn [in seen x y groups]
-     (let [ found (explore-group in seen x y) ]
-       (if (> y 127) groups
-         (recur in found (mod (inc x) 128) (if (= x 127) (inc y) y) (if (not= (count seen) (count found)) (inc groups) groups)))))
+    (let [ res (explore-group in seen x y groups) ]
+      (if (> y 127) groups
+        (recur (first res) (second res) (mod (inc x) 128) (if (= x 127) (inc y) y) (if (not= (count seen) (count (second res))) (inc groups) groups)))))
     in #{} 0 0 0))
 
 ;(core/do-parts part1 part2 grid)
