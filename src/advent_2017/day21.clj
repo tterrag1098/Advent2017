a6173085803c4a43f9fb0cf53a90c6fe5cee9010
tterrag <tterrag1098@gmail.com>
2017-12-21 13:32:28 -0500

    Finished day 21

--- src/advent_2017/day21.clj
+++ src/advent_2017/day21.clj
@@ -4,14 +4,24 @@
 
 (def input 
   (->> (core/read-input "day21.txt")
-        (map (fn [l] 
-               (-> l
-                   (str/split #"\s=>\s")
-                   (->> (map (fn [r] 
-                               (-> r 
-                                   (str/split #"/")
-                                   (->> (map #(vec (char-array %))))
-                                   (vec))))) (vec)))) (vec)))
+       (map (fn [l] 
+              (-> l
+                  (str/split #"\s=>\s")
+                  (->> (map (fn [r] 
+                              (-> r 
+                                  (str/split #"/")
+                                  (->> (map #(vec (char-array %))))
+                                  (vec))))) (vec)))) (vec)))
+
+(def rules
+  (apply concat (map (fn [rule]
+                       (let [m (first rule) ]
+                         (as-> m $
+                               (vector $ (flip $ true))
+                               (iterate (partial map rotate) $)
+                               (take 4 $)
+                               (apply concat $)
+                               (map #(vector % (second rule)) $)))) input)))
 
 (def grid
 [ [ \. \# \. ]
@@ -25,40 +35,40 @@
   (if updn (vec (reverse mat)) (vec (map #(vec (reverse %)) mat))))
 
 (defn find-rule [rules grid]
-  (first (filter (fn [rule] 
-                   (let [ m (first rule) ]
-                     (as-> m $
-                           (vector $ (flip $ true) (flip $ false))
-                           (iterate #(map rotate %) $)
-                           (take 4 $)
-                           (apply concat $)
-                           (some #{grid} $)))) rules)))
+  (first (filter #(= grid (first %)) rules)))
 
-(defn vec-split [idx v] [(subvec v 0 idx) (subvec v idx)])
+(defn vec-split [idx v] (if (>= idx (count v)) v [(subvec v 0 idx) (subvec v idx)]))
 
 (defn partition-mat [m row col] 
-  (mapcat (partial apply map vector) 
-          (vec-split row (mapv (partial vec-split col) m))))
+  (as-> m $
+     (mapcat (partial apply map vector)
+             (partition row (mapv (partial partition col) $)))
+     (partition (/ (count m) col) $)))
 
-(defn merge-halves [m1 m2]
-  (vec (map-indexed #(vec (concat %2 (nth m2 %1))) m1)))
+(defn merge-horiz [mats]
+  (vec 
+    (map-indexed 
+      (fn [i m] (vec 
+                  (apply concat m
+                         (map #(nth % i) (rest mats))))) 
+                (first mats))))
 
-(defn merge-corners [m1 m2 m3 m4]
-  (concat (merge-halves m1 m2) (merge-halves m3 m4)))
+(defn merge-corners [mats]
+  (apply concat (map merge-horiz mats)))
 
 (defn enhance
   ([rules grid times] (last (take (inc times) (iterate #(enhance rules %) grid))))
   ([rules grid]
-    (let [ half (/ (count grid) 2) ]
-      (if (>= half 2)
-        (apply merge-corners (map #(enhance rules %) (partition-mat grid half half)))
-        (->> grid
-             (find-rule rules)
-             second)))))
+    (if (>= (/ (count grid) 2) 2)
+      (let [ split (if (zero? (mod (count grid) 2)) 2 3) ]
+        (merge-corners (map #(map (partial enhance rules) %) (partition-mat grid split split))))
+      (->> grid
+           (find-rule rules)
+           second))))
 
-(defn part1 [in] (count (filter #(= \# %) (flatten (enhance in grid 5)))))
+(defn on-after [rules amt] (count (filter (partial = \#) (flatten (enhance rules grid amt)))))
 
-(defn part2 [in]
-  )
+(defn part1 [in] (on-after in 5))
+(defn part2 [in] (on-after in 18))
 
-(core/do-parts part1 part2 input)
+(core/do-parts part1 part2 rules)
