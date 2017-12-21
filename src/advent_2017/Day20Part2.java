9937eedba853f24ad346180d68101899af1c90a0
tterrag <tterrag1098@gmail.com>
2017-12-20 23:18:32 -0500

    Day 20 part 2

---
+++ src/advent_2017/Day20Part2.java
@@ -0,0 +1,54 @@
+package advent_2017;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day20Part2 {
+
+	public static void main(String[] args) throws IOException {
+		List<String> lines = Files.readAllLines(Paths.get("resources", "temp.txt"));
+		List<int[][]> data = lines.stream()
+				     .map(s -> s.replaceAll("[pva<>=]", ""))
+				     .map(s -> s.replaceAll(",", " "))
+				     .map(s -> s.split("\\s+"))
+				     .map(arr -> Arrays.stream(arr).mapToInt(Integer::parseInt).toArray())
+				     .map(a -> new int[][] { { a[0], a[1], a[2] },
+				    		        	     { a[3], a[4], a[5] },
+				    		        	     { a[6], a[7], a[8] } })
+				     .collect(Collectors.toList());
+		
+		for (int i = 0; i < 10000; i++) {
+			// simulate
+			for (int[][] p : data) {
+				for(int xyz = 0; xyz < 3; xyz++) {
+					p[1][xyz] += p[2][xyz];
+					p[0][xyz] += p[1][xyz];
+				}
+			}
+			
+			// remove collisions
+			for (int j = 0; j < data.size(); j++) {
+				int[][] p = data.get(j);
+				boolean removed = false;
+				for (int k = 0; k < data.size(); k++) {
+					int[][] p2 = data.get(k);
+					if (p != p2 && Arrays.equals(p[0], p2[0])) {
+						if (!removed) {
+							data.remove(j--);
+							removed = true;
+							k--;
+						}
+						data.remove(k--);
+					}
+				}
+			}
+		}
+		
+		System.out.println(data.size());
+	}
+
+}
--- src/advent_2017/day20.clj
+++ src/advent_2017/day20.clj
@@ -1,4 +1,5 @@
-(ns advent-2017.day20)
+(ns advent-2017.day20
+  (:import (advent_2017 Day20Part2)))
 (require '[clojure.string :as str])
 (require '[advent-2017.core :as core])
 
@@ -15,19 +16,35 @@
                        (zipmap [:p :v :a])))))
        (vec)))
 
+(defn abs [n] (if (< n 0) (- n) n))
+(defn mag [v] (apply + (map abs v)))
+
 (defn find-closest [particles]
-  (apply min-key (fn [p] (apply + (map #(Math/abs %) (p :p)))) particles))
+  (apply min-key #(mag (% :p)) particles))
 
-;; Infinite loop because lazy, once it prints out the same value for a long time it can be assumed that this is the answer
+(defn index-of [particles p] ((clojure.set/map-invert (into {} (map-indexed #(vector %1 %2) particles))) p))
+
+(defn simulate-one [in]
+  (map (fn [p] 
+         (-> p
+             (assoc :v (map-indexed #(+ %2 (nth (p :a) %1)) (p :v)))
+             (assoc :p (map-indexed #(+ %2 (nth (p :v) %1)) (p :p)))))
+       in))
+
+(defn simulate [in amt]
+  (if (zero? amt) in
+    (recur (simulate-one in) (dec amt))))
+
+(defn find-mins [particles k]
+  (as-> particles $ 
+        (mag ((apply min-key #(mag (% k)) $) k))
+        (filter #(= (mag (% k)) $) particles)))
+
 (defn part1 [in]
-  (do (prn ((clojure.set/map-invert (into {} (map-indexed #(vector %1 %2) in))) (find-closest in)))
-      (recur (map (fn [p] 
-                    (-> p
-                         (assoc :v (map-indexed #(+ %2 (nth (p :a) %1)) (p :v)))
-                         (assoc :p (map-indexed #(+ %2 (nth (p :v) %1)) (p :p)))))
-                  in)))) 
+  (as-> in $
+        (simulate $ 1000)
+        (index-of $ (find-closest $))))
 
-(defn part2 [in]
-  )
+(defn part2 [in] "Run the Day20Part2 Java program!")
 
-;(core/do-parts part1 part2 input)
+(core/do-parts part1 part2 input)
